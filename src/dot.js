b32f90c44931a8c8de74af00e0e1b844f4d2f1da
Srikanth K Hari <srikanthkhari@gmail.com>
2016-07-03 13:07:37 -0700

    upgrade to 0.28 with minor UI changes

--- src/dot.js
+++ src/dot.js
@@ -1,38 +1,34 @@
-'use strict'
-
+'use strict';
 import React, {
-  Component,
-  View,
-} from 'react-native'
+    Component
+} from 'react';
+import { View } from 'react-native';
 
 export default class Dot extends Component {
-  static propTypes = {
-    color: React.PropTypes.string,
-    diameter: React.PropTypes.number,
-    style: View.propTypes.style,
-  };
-
-  static defaultProps = {
-    color: 'rgba(192, 192, 192, 1)',
-    diameter: 10,
-  };
 
   render() {
-    const { color, diameter } = this.props
+    const { color, diameter } = this.props;
 
-    return (
-      <View
-        style={[{
-          backgroundColor: color,
-          width: diameter,
-          height: diameter,
-          borderRadius: diameter / 2,
-          marginLeft: 3,
-          marginRight: 3,
-          marginTop: 3,
-          marginBottom: 3,
-        }, this.props.style]}
-      />
-    )
+    return React.createElement(View, {
+      style: [{
+        backgroundColor: color,
+        width: diameter,
+        height: diameter,
+        borderRadius: diameter / 2,
+        marginLeft: 3,
+        marginRight: 3,
+        marginTop: 3,
+        marginBottom: 3
+      }, this.props.style]
+    });
   }
 }
+Dot.propTypes = {
+  color: React.PropTypes.string,
+  diameter: React.PropTypes.number,
+  style: View.propTypes.style
+};
+Dot.defaultProps = {
+  color: 'rgba(255, 255, 255, .5)',
+  diameter: 10
+};
